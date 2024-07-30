206690a5827431179e7c4b23888d10ef04a27d9c
checkman3498 <chrisheckman8000@gmail.com>
2024-07-30 08:39:46 -0500

    Show Loading Skeletons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,22 +1,29 @@
-import useGames from '../hooks/useGames'
-import { list, SimpleGrid, Text } from '@chakra-ui/react';
-import GameCard from './GameCard';
-import { Game } from '../hooks/useGames';
-
+import useGames from "../hooks/useGames";
+import { list, SimpleGrid, Text } from "@chakra-ui/react";
+import GameCard from "./GameCard";
+import { Game } from "../hooks/useGames";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
-
-    const { games, error } = useGames();
+  const { games, error, isLoading } = useGames();
+  const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
     <>
-        {error && <Text>{error}</Text>}
-        <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 5}} padding='10px' spacing={10}>
-            {games.map((game) => (
-                <GameCard key={game.id} game={game} />))}
-        </SimpleGrid>
+      {error && <Text>{error}</Text>}
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        padding="10px"
+        spacing={10}
+      >
+        {isLoading &&
+          skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
+        {games.map((game) => (
+          <GameCard key={game.id} game={game} />
+        ))}
+      </SimpleGrid>
     </>
-  )
-}
+  );
+};
 
 export default GameGrid;
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -25,21 +25,30 @@ const useGames = () => {
 
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState('');
+    const [isLoading, setLoading] = useState(false);
 
     useEffect(() => {
         const controller = new AbortController
+
+        setLoading(true)
         // This will create the API Client Call with the Abort call
-        apiClient.get<FetchGamesResponse>('/games', {signal: controller.signal})
-            .then(res => setGames(res.data.results))
+        apiClient
+            .get<FetchGamesResponse>('/games', {signal: controller.signal})
+            .then((res) => {
+                setGames(res.data.results)
+                setLoading(false)
+            })
             .catch(err => {
                 if (err instanceof CanceledError) return;
-                setError(err.message)});
+                setError(err.message)
+                setLoading(false)
+            });
 
         // Create the cleanup function
         return () => controller.abort();
     } , []);
 
-    return { games, error };
+    return { games, error, isLoading };
 }
 
 export default useGames;
